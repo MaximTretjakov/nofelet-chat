c132029169ceba6724739a8873244f1bbbff1777
Maxim Tretyakov <tretiakov.m@bookcentre.ru>
2026-05-18 20:01:52 +0300

    [feature/flow] Добавил управление горутинами записи и чтения

--- internal/domain/usecase/chat.go
+++ internal/domain/usecase/chat.go
@@ -1,8 +1,12 @@
 package usecase
 
 import (
+	"context"
 	"encoding/json"
 	"log/slog"
+	"time"
+
+	"github.com/gorilla/websocket"
 
 	"nofelet/decorator"
 	"nofelet/internal/domain/chat/controller/view"
@@ -10,47 +14,143 @@ import (
 )
 
 const (
-	joinEvent = "join"
-	textEvent = "text"
+	joinEvent  = "join"
+	textEvent  = "text"
+	pongWait   = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10
 )
 
-func (uc *UseCase) Chat(cm *decorator.ConnectionManager, chat *singleton.Chat) error {
-	go handler(cm, chat, uc.log) // todo реализовать контроль за горутинами (в каких случаях оа завершается и как)
+func (uc *UseCase) Chat(ctx context.Context, cm *decorator.ConnectionManager, chat *singleton.Chat) error {
+	ctx, cancel := context.WithCancel(ctx)
 
-	return nil
-}
+	defer func() {
+		cancel()
+		if err := cm.Close(); err != nil {
+			uc.log.Error("defer close connection chat", "err", err)
+		}
+	}()
 
-// handler - обрабатывает логику обмена сообщениями
-func handler(cm *decorator.ConnectionManager, chat *singleton.Chat, log *slog.Logger) {
 	var e view.Event
+	dataCh := make(chan view.Event, 100)
+
+	go writePump(ctx, dataCh, cm, chat, uc.log)
+	go readPump(ctx, dataCh, cm, chat, uc.log)
 
 	for {
 		if readErr := cm.ReadJSON(&e); readErr != nil {
-			log.Error("socket read json", "err", readErr)
-			break
+			uc.log.Error("socket read json", "err", readErr)
+			return readErr
 		}
 
-		switch e.Type {
-		case joinEvent:
-			var j view.JoinMessagePayload
-			if err := json.Unmarshal(e.Payload, &j); err != nil {
-				log.Error("failed to unmarshal message payload", "err", err)
-				continue // эту ошибку обработали, идем читать следующее сообщение
+		dataCh <- e
+	}
+}
+
+func readPump(
+	ctx context.Context,
+	dataCh chan view.Event,
+	cm *decorator.ConnectionManager,
+	chat *singleton.Chat,
+	log *slog.Logger,
+) {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		cm.Close()
+	}()
+
+	// ... тут настройка Ping/Pong таймаутов из Кейса 1 ...
+
+	for {
+		select {
+		case msg := <-dataCh:
+			// Пришло сообщение для отправки клиенту
+			if err := dispatcher(msg, cm, chat, log); err != nil {
+				return
+			}
+		case <-ticker.C:
+			// Время слать Ping фронтенду, чтобы проверить, жив ли он
+			if err := cm.SetWriteDeadline(time.Now().Add(10 * time.Second)); err != nil {
+				return
 			}
 
-			if jErr := join(cm, chat, j); jErr != nil {
-				log.Error("join handler", "err", jErr)
+			if err := cm.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
 			}
-		case textEvent:
-			var m view.SendMessagePayload
-			if err := json.Unmarshal(e.Payload, &m); err != nil {
-				log.Error("failed to unmarshal typing payload", "err", err)
-				continue
-			}
-
-			if tErr := text(chat, m); tErr != nil {
-				log.Error("handler", "err", tErr)
+		case <-ctx.Done():
+			// Сигнал от Graceful Shutdown сервера
+			if err := cm.WriteMessage(
+				websocket.CloseMessage,
+				websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Server closed connection"),
+			); err != nil {
+				return
 			}
 		}
 	}
+}
+
+func writePump(
+	ctx context.Context,
+	dataCh chan view.Event,
+	cm *decorator.ConnectionManager,
+	chat *singleton.Chat,
+	log *slog.Logger,
+) {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		cm.Close()
+	}()
+
+	for {
+		select {
+		case msg := <-dataCh:
+			// Пришло сообщение для отправки клиенту
+			if err := dispatcher(msg, cm, chat, log); err != nil {
+				return
+			}
+		case <-ticker.C:
+			// Время слать Ping фронтенду, чтобы проверить, жив ли он
+			if err := cm.SetWriteDeadline(time.Now().Add(10 * time.Second)); err != nil {
+				return
+			}
+
+			if err := cm.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		case <-ctx.Done():
+			// Сигнал от Graceful Shutdown сервера
+			if err := cm.WriteMessage(
+				websocket.CloseMessage,
+				websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Server closed connection"),
+			); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func dispatcher(e view.Event, cm *decorator.ConnectionManager, chat *singleton.Chat, log *slog.Logger) error {
+	switch e.Type {
+	case joinEvent:
+		var j view.JoinMessagePayload
+		if err := json.Unmarshal(e.Payload, &j); err != nil {
+			log.Error("failed to unmarshal message payload", "err", err)
+		}
+
+		if jErr := join(cm, chat, j); jErr != nil {
+			log.Error("join handler", "err", jErr)
+		}
+	case textEvent:
+		var m view.SendMessagePayload
+		if err := json.Unmarshal(e.Payload, &m); err != nil {
+			log.Error("failed to unmarshal typing payload", "err", err)
+		}
+
+		if tErr := text(chat, m); tErr != nil {
+			log.Error("handler", "err", tErr)
+		}
+	}
+
+	return nil
 }
